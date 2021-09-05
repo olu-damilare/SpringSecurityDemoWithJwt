0ec709970cc97fc79fcf81e8a6cfd49122d3f936
Damilare Jolayemi <djolayemi@gmail.com>
2021-09-05 21:51:41 +0100

    update

--- src/main/java/com/example/springBootSecurityDemo/auth/ApplicationUserDAO.java
+++ src/main/java/com/example/springBootSecurityDemo/auth/ApplicationUserDAO.java
@@ -1,7 +1,5 @@
 package com.example.springBootSecurityDemo.auth;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
